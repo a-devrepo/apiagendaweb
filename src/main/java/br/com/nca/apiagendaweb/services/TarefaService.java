653ae3a9c2dbc99f0a514e576bf4895aebcaa8a6
alison <adevpr11@gmail.com>
2026-03-18 11:19:26 -0300

    feat: atualiza TarefaService com método para cadastrar Tarefa

--- src/main/java/br/com/nca/apiagendaweb/services/TarefaService.java
+++ src/main/java/br/com/nca/apiagendaweb/services/TarefaService.java
@@ -1,17 +1,77 @@
 package br.com.nca.apiagendaweb.services;
 
 
+import br.com.nca.apiagendaweb.dtos.CategoriaResponse;
+import br.com.nca.apiagendaweb.dtos.TarefaRequest;
+import br.com.nca.apiagendaweb.dtos.TarefaResponse;
+import br.com.nca.apiagendaweb.entities.Tarefa;
+import br.com.nca.apiagendaweb.enums.Prioridade;
+import br.com.nca.apiagendaweb.repositories.CategoriaRepository;
 import br.com.nca.apiagendaweb.repositories.TarefaRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class TarefaService {
 
     private TarefaRepository tarefaRepository;
+    private CategoriaRepository categoriaRepository;
 
-    public TarefaService(TarefaRepository tarefaRepository) {
+    public TarefaService(TarefaRepository tarefaRepository, CategoriaRepository categoriaRepository) {
         this.tarefaRepository = tarefaRepository;
+        this.categoriaRepository = categoriaRepository;
     }
 
+    public TarefaResponse cadastrar(TarefaRequest request) {
 
+        var categoria = categoriaRepository.findById(request.getCategoriaId());
+
+        if(categoria.isEmpty()){
+            throw new IllegalArgumentException("A categoria informada não existe, verifique o ID");
+        }
+
+        var tarefa = toTarefa(request);
+
+        tarefa.setCategoria(categoria.get());
+
+        tarefaRepository.save(tarefa);
+
+        return toTarefaResponse(tarefa);
+    }
+
+    private Tarefa toTarefa(TarefaRequest request) {
+        var tarefa = new Tarefa();
+
+        tarefa.setNome(request.getNome());
+
+        var dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        tarefa.setData(LocalDate.parse(request.getData(),dateFormatter));
+
+        var horaForatter = DateTimeFormatter.ofPattern("HH:mm");
+        tarefa.setHora(LocalTime.parse(request.getHora(),horaForatter));
+
+        tarefa.setPrioridade(Prioridade.valueOf(request.getPrioridade()));
+
+        return tarefa;
+    }
+
+    private TarefaResponse toTarefaResponse(Tarefa tarefa) {
+
+        var response = new TarefaResponse();
+
+        response.setId(tarefa.getId());
+        response.setNome(tarefa.getNome());
+        response.setData(tarefa.getData().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        response.setData(tarefa.getHora().format(DateTimeFormatter.ofPattern("HH:mm")));
+        response.setPrioridade(tarefa.getPrioridade().toString());
+
+        response.setCategoria(new CategoriaResponse());
+        response.getCategoria().setId(tarefa.getCategoria().getId());
+        response.getCategoria().setNome(tarefa.getCategoria().getNome());
+
+        return response;
+    }
 }
