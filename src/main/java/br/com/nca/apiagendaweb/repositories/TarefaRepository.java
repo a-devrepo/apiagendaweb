654b250903610271f878b8f01666b0f83112f06e
alison <adevpr11@gmail.com>
2026-03-18 11:17:44 -0300

    fix: altera entidade do Repository

--- src/main/java/br/com/nca/apiagendaweb/repositories/TarefaRepository.java
+++ src/main/java/br/com/nca/apiagendaweb/repositories/TarefaRepository.java
@@ -1,11 +1,12 @@
 package br.com.nca.apiagendaweb.repositories;
 
 import br.com.nca.apiagendaweb.entities.Categoria;
+import br.com.nca.apiagendaweb.entities.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TarefaRepository extends JpaRepository<Categoria, UUID> {
+public interface TarefaRepository extends JpaRepository<Tarefa, UUID> {
 }
