084aa380c7ce0b2935b236464d52e6f4e0483555
alison <adevpr11@gmail.com>
2026-03-18 11:18:32 -0300

    feat: atualiza DTO de Tarefa

--- src/main/java/br/com/nca/apiagendaweb/dtos/TarefaResponse.java
+++ src/main/java/br/com/nca/apiagendaweb/dtos/TarefaResponse.java
@@ -1,4 +1,18 @@
 package br.com.nca.apiagendaweb.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class TarefaResponse {
+
+    private UUID id;
+    private String nome;
+    private String data;
+    private String hora;
+    private String prioridade;
+    private CategoriaResponse categoria;
 }
